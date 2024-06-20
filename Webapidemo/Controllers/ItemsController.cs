a1deb2d9d9c9b76305dd2a8e6f3b33b1d195c68a
divyanshraj467 <divyanshraj1@gmail.com>
2024-06-20 13:03:38 +0530

    Add project files.

---
+++ Webapidemo/Controllers/ItemsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webapidemo.Controllers
+{
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/items")]
+        public string GetItems()
+        {
+            return "Read all the items";
+        }
+        [HttpGet]
+        [Route("/items/{id}")]
+        public string GetItemsbyid(int id)
+        {
+            return $"read item number:{id}";
+        }
+        [HttpPost]
+        [Route("/item")]
+
+        public string PostItems()
+        {
+            return "create item";
+        }
+        [HttpPut]
+        [Route("/item")]
+        public string PutItems()
+        {
+            return "update items";
+        }
+        [HttpDelete]
+        [Route("/item")]
+        public string DeleteItems(int id)
+        {
+            return $"delete item {id}";
+        }
+    }
+
+
+}
