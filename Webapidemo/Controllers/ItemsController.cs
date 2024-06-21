361dd81f6fba41694ff4357857962f513eaeadc7
divyanshraj467 <divyanshraj1@gmail.com>
2024-06-21 12:48:34 +0530

    commit

--- Webapidemo/Controllers/ItemsController.cs
+++ Webapidemo/Controllers/ItemsController.cs
@@ -3,35 +3,31 @@
 namespace Webapidemo.Controllers
 {
     [ApiController]
+    [Route ("/api[Controller]")]
     public class ItemsController : ControllerBase
     {
         [HttpGet]
-        [Route("/items")]
         public string GetItems()
         {
             return "Read all the items";
         }
-        [HttpGet]
-        [Route("/items/{id}")]
+        [HttpGet("{id}")]
         public string GetItemsbyid(int id)
         {
             return $"read item number:{id}";
         }
         [HttpPost]
-        [Route("/item")]
-
         public string PostItems()
         {
             return "create item";
         }
+        
         [HttpPut]
-        [Route("/item")]
         public string PutItems()
         {
             return "update items";
         }
-        [HttpDelete]
-        [Route("/item")]
+        [HttpDelete("{id}")]
         public string DeleteItems(int id)
         {
             return $"delete item {id}";
---
+++ Webapidemo/Models/Items.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webapidemo.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Brand { get; set; }
+        [Required]
+        public string? Color { get; set; }
+        public string Size { get; set; }
+        [Required]
+        public string? Gender { get; set; }
+        public double Price { get; set; }
+
+    } 
+}
